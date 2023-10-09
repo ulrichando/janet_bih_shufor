eec79afeeee595aa29bfc85782df33f821ec8ed7
Ulrich Ando <ulrichando007@gmail.com>
2023-10-09 04:29:13 -0400

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
   return (
     <div className='relative h-screen bg-cover bg-center bg-no-repeat z-10 flex flex-col justify-center' style={{ backgroundImage: 'url("src/images/janet_bih_fofang2.jpg")', backgroundSize: 'cover', backgroundPosition: 'top', }}>
-      <button className='text-3xl md:text-7xl text-white rounded-full absolute pb-5 md:pb-15' style={{ top: '80%', left: '50%', transform: 'translate(-50%, -50%)', '@media (max-width: 600px)': { top: '80%' } }}>
+      <button className='text-3xl md:text-7xl text-white rounded-full absolute pb-5 md:pb-15' style={{ top: '78%', left: '50%', transform: 'translate(-50%, -50%)', '@media (max-width: 600px)': { top: '80%' } }}>
         Welcome to my portfolio website
       </button>
       <button className='text-1xl text-white rounded-full absolute hidden md:block' style={{ top: '88%', left: '50%', transform: 'translate(-50%, 50%)' }} onClick={handleScrollDown}>
