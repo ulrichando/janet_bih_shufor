0ac5187e34279d8e0c1845529c0deedde8952f77
Ulrich Ando <ulrichando007@gmail.com>
2023-10-09 04:34:03 -0400

    backgound image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import backgroundImage from '../images/janet_bih_fofang2.jpg';
 
 const Header = () => {
   const handleScrollDown = () => {
@@ -10,8 +11,8 @@ const Header = () => {
   };
 
   return (
-    <div className='relative h-screen bg-cover bg-center bg-no-repeat z-10 flex flex-col justify-center' style={{ backgroundImage: 'url("src/images/janet_bih_fofang2.jpg")', backgroundSize: 'cover', backgroundPosition: 'top', }}>
-      <button className='text-3xl md:text-7xl text-white rounded-full absolute pb-5 md:pb-15' style={{ top: '75%', left: '50%', transform: 'translate(-50%, -50%)', '@media (max-width: 600px)': { top: '80%' } }}>
+    <div className='relative h-screen bg-cover bg-center bg-no-repeat z-10 flex flex-col justify-center' style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'top' }}>
+      <button className='text-3xl md:text-7xl text-white rounded-full absolute pb-5 md:pb-10' style={{ top: '70%', left: '50%', transform: 'translate(-50%, -50%)', '@media (max-width: 600px)': { top: '80%' } }}>
         Welcome to my portfolio website
       </button>
       <button className='text-1xl text-white rounded-full absolute hidden md:block' style={{ top: '88%', left: '50%', transform: 'translate(-50%, 50%)' }} onClick={handleScrollDown}>
