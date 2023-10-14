9a388a905f3b7bfd86b3c746ba7a968c6194f958
Ulrich Ando <ulrichando007@gmail.com>
2023-10-14 07:42:49 -0400

    update

--- src/Pages/PublicationPage.jsx
+++ src/Pages/PublicationPage.jsx
@@ -1,24 +1,7 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Sidenav from '../components/Sidenav'
-import Subscribe from '../components/Subscribe'
+import React from "react";
 
-const ResearchPage = () => {
-  return (
-    <div>  
-      <Navbar />
-      <Sidenav />
-       <div className="container mx-auto mt-10">
-      <h2 className="text-7xl font-bold mb-6 text-center py-10">Blog post</h2>
-      <section className="bg-black py-50 p-3 text-white pt-40">
-          <h2 className="text-8xl font-bold mb-6 text-center py-40">COMING SOON ...</h2>
-      </section>
-      </div>
-      <Subscribe />
-      <Footer />
-    </div>
-  )
-}
+const PublicationPage = () => {
+  return <div>PublicationPage</div>;
+};
 
-export default ResearchPage
+export default PublicationPage;
--- src/Pages/ResearchPage.jsx
+++ src/Pages/ResearchPage.jsx
@@ -1,78 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Sidenav from "../components/Sidenav";
 import Subscribe from "../components/Subscribe";
+import "/src/css/Research.css";
 
-const researchProjects = [
-  {
-    title: "Representations in Robotics",
-    description: "Description for Project 1",
-    findings: "Key findings for Project 1",
-  },
-  {
-    title: "Computational Thinking",
-    description: "Description for Project 2",
-    findings: "Key findings for Project 2",
-  },
-  {
-    title: "Learning Sciences",
-    description: "Description for Project 3",
-    findings: "Key findings for Project 3",
-  },
-  {
-    title: "Artificial Intelligence",
-    description: "Description for Project 4",
-    findings: "Key findings for Project 4",
-  },
-];
+function ResearchPage() {
+  const [cards] = useState([
+    {
+      title: "Representation in Robotics",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Exercitationem nesciunt voluptatem ratione saepe corporis adveritatis eveniet harum ut iure laudantium qui animi eaque ataccusantium tempora Veniam voluptatum illum.",
+    },
+    {
+      title: "Computaitonal Thinking",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Exercitationem nesciunt voluptatem ratione saepe corporis adveritatis eveniet harum ut iure laudantium qui animi eaque ataccusantium tempora Veniam voluptatum illum.",
+    },
+    {
+      title: "Learning Sciences",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Exercitationem nesciunt voluptatem ratione saepe corporis adveritatis eveniet harum ut iure laudantium qui animi eaque ataccusantium tempora Veniam voluptatum illum.",
+    },
+    {
+      title: "Artificial Intelligence",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Exercitationem nesciunt voluptatem ratione saepe corporis adveritatis eveniet harum ut iure laudantium qui animi eaque ataccusantium tempora Veniam voluptatum illum.",
+    },
+  ]);
 
-const ResearchPage = () => {
   return (
     <div>
       <Navbar />
       <Sidenav />
-      <div className="container mx-auto mt-10">
-        <h2 className="text-7xl font-bold mb-6 text-center py-10">Research</h2>
-        <section className="container mx-auto p-5 text-black">
-          <div className="container mx-auto p-5">
-            <h1 className="text-3xl font-bold mb-5 text-[#E4CB86]">Topics</h1>
-
-            <div className="flex flex-wrap -mx-4">
-              {researchProjects.map((project, index) => (
-                <div
-                  key={index}
-                  className="w-full md:w-1/2 lg:w-1/2 xl:w-1/4 px-8 mb-8"
-                >
-                  <div className="bg-[#ffffff] rounded shadow p-6 h-full">
-                    <h2 className="text-xl font-bold mb-2 text-center bg-[#ffffff] text-black">
-                      {project.title}
-                    </h2>
-                    <div className="text-black bg-[#ffffff]">
-                      <p className="text-black bg-[#ffffff]">
-                        <strong className="text-black bg-[#ffffff]">
-                          Description:
-                        </strong>{" "}
-                        {project.description}
-                      </p>
-                      <p className="text-black bg-[#ffffff]">
-                        <strong className="text-black bg-[#ffffff]">
-                          Key Findings:
-                        </strong>{" "}
-                        {project.findings}
-                      </p>
-                    </div>
-                  </div>
+      <div className="container mx-auto mt-1 content pb-20 flex flex-col justify-center items-center">
+        <h2 className="text-4xl md:text-7xl font-bold mb-10 text-center mt-1 text-white py-10">
+          RESEARCH
+        </h2>
+        <div className="py-20">
+          <div className="container">
+            <div className="cards">
+              {cards.map((card, i) => (
+                <div key={i} className="card">
+                  <h3>{card.title}</h3>
+                  <p>{card.text}</p>
+                  <button className="btn"> Explore</button>
                 </div>
               ))}
             </div>
           </div>
-        </section>
+        </div>
       </div>
       <Subscribe />
       <Footer />
     </div>
   );
-};
+}
 
 export default ResearchPage;
