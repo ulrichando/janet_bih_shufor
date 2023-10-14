6351e7406aebbf609aca3e654dffe0e5ce7c03e6
Ulrich Ando <ulrichando007@gmail.com>
2023-10-14 07:47:48 -0400

    update

--- src/Pages/PublicationPage.jsx
+++ src/Pages/PublicationPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Sidenav from "../components/Sidenav";
+import "/src/css/ContactPage.css";
+import Subscribe from "../components/Subscribe";
 
 const PublicationPage = () => {
-  return <div>PublicationPage</div>;
+  return (
+    <div>
+      <Navbar />
+      <Sidenav />
+      <div className="container mx-auto mt-1 content pb-20 flex flex-col justify-center items-center">
+        <h2 className="text-4xl md:text-7xl font-bold mb-10 text-center mt-1 text-white py-5">
+          Blog Post
+        </h2>
+      </div>
+      <Subscribe />
+      <Footer />
+    </div>
+  );
 };
 
 export default PublicationPage;
