a7ba2517cc43711165e818ebc288aecd89402c49
Ulrich Ando <ulrichando007@gmail.com>
2023-10-14 02:56:24 -0400

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-import backgroundImage from '../images/janet_bih_fofang2.jpg';
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import backgroundImage from "../images/janet_bih_fofang2.jpg";
 
 const Header = () => {
   const handleScrollDown = () => {
     window.scrollTo({
       top: window.pageYOffset + 1400,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
-    <div className='relative h-screen bg-cover bg-center bg-no-repeat z-10 flex flex-col justify-center' style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'top' }}>
-      <button className='text-4xl md:text-8xl text-white rounded-full absolute pb-10 md:pb-5' style={{ top: '77%', left: '50%', transform: 'translate(-50%, -50%)', '@media (max-width: 600px)': { top: '80%' } }}>
-        Welcome to my portfolio website
+    <div
+      className="relative h-screen bg-cover bg-center bg-no-repeat z-10 flex flex-col justify-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+      }}
+    >
+      <button
+        className="text-4xl md:text-8xl text-white rounded-full absolute pb-10 md:pb-5"
+        style={{
+          top: "77%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          "@media (max-width: 600px)": { top: "80%" },
+        }}
+      >
+        Welcome To My Portfolio Website
       </button>
-      <button className='text-1xl text-white rounded-full absolute hidden md:block' style={{ top: '88%', left: '50%', transform: 'translate(-50%, 50%)' }} onClick={handleScrollDown}>
+      <button
+        className="text-1xl text-white rounded-full absolute hidden md:block"
+        style={{ top: "88%", left: "50%", transform: "translate(-50%, 50%)" }}
+        onClick={handleScrollDown}
+      >
         SCROLL DOWN
       </button>
     </div>
